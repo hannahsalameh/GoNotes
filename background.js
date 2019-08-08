34aa8dfabc13d5cc68cc21228670a77a8c56158f
Hannah Salameh <hannahsalameh18@gmail.com>
2019-08-08 15:30:08 -0700

    deleted unneccesary popup.js info

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
+      console.log("This bitch working!!!");
     });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
